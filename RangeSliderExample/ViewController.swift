76275494f44c2f2011164070292667ed0e385c4c
AlexanderNaumov <haymob@me.com>
2016-02-13 19:46:16 +0300

    Initial Commit

---
+++ RangeSliderExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RangeSliderExample
+//
+//  Created by Alexander Naumov on 13.02.16.
+//  Copyright © 2016 Alexander Naumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
